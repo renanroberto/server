c4c9de7f85ec8069de00c8f84816bc9373cdaace
Renan <renan@grama.cc>
2018-06-29 19:27:05 -0300

    working; TODO: refactor and error handling

--- handler/login.go
+++ handler/login.go
@@ -1,19 +1,18 @@
 package handler
 
 import (
-	"crypto/hmac"
-	"crypto/sha256"
-	"encoding/base64"
 	"encoding/json"
 	"net/http"
 )
 
+const secret = "qualquer coisa"
+
 type User struct {
 	Email    string `json:"email"`
-	Password string `json:"password"`
+	Password string `json:"password,omitempty"`
 }
 
-type Auth struct {
+type TokenGen struct {
 	Token string `json:"token"`
 }
 
@@ -21,7 +20,7 @@ func (user *User) JSONRead(r *http.Request) error {
 	return json.NewDecoder(r.Body).Decode(&user)
 }
 
-func (auth Auth) JSONWrite(w http.ResponseWriter) error {
+func (auth TokenGen) JSONWrite(w http.ResponseWriter) error {
 	return json.NewEncoder(w).Encode(auth)
 }
 
@@ -39,49 +38,12 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	token := jwt(user)
-	auth := Auth{token}
+	token := JWTencode(user, secret)
+	auth := TokenGen{token}
 
 	err = auth.JSONWrite(w)
 	if err != nil {
 		http.Error(w, "Error on generate token", 500)
 		return
 	}
-}
-
-func jwt(user User) string {
-	type Header struct {
-		Typ string `json:"typ"`
-		Alg string `json:"alg"`
-	}
-
-	type Payload struct {
-		Email string `json:"email"`
-	}
-
-	header := toString(Header{"JWT", "SH256"})
-	payload := toString(Payload{user.Email})
-
-	data := base64encode(header) + "." + base64encode(payload)
-	signature := hs256(data, "secrete-key")
-
-	return data + "." + signature
-}
-
-func toString(src interface{}) string {
-	str, _ := json.Marshal(src)
-	return string(str)
-}
-
-func base64encode(src string) string {
-	data := []byte(src)
-	str := base64.StdEncoding.EncodeToString(data)
-	return str
-}
-
-func hs256(src, secret string) string {
-	key := []byte(secret)
-	hash := hmac.New(sha256.New, key)
-	hash.Write([]byte(src))
-	return base64.StdEncoding.EncodeToString(hash.Sum(nil))
 }
--- router/routes.go
+++ router/routes.go
@@ -9,4 +9,5 @@ type Routes []Route
 var routes = Routes{
 	{"Home", "/", []string{"GET"}, handler.Index, true},
 	{"Login", "/login", []string{"POST"}, handler.Login, true},
+	{"Auth", "/auth", []string{"GET"}, handler.Auth, true},
 }
